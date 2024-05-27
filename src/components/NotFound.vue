8cb407e5781c5e924d884f1aa86714d218ce3497
Zenara12 <nolipernes@gmail.com>
2024-05-27 10:51:28 +0800

    add:page not found

---
+++ src/components/NotFound.vue
@@ -0,0 +1,8 @@
+<template>
+    <v-empty-state
+      icon="mdi-magnify"
+      text="The page you were looking for does not exist"
+      title="Page not found"
+      headline="Whoops, 404"
+    ></v-empty-state>
+  </template>
